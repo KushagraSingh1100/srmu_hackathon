442925a4a585d1d735dcf75f352779aaa24f90bc
Kushagra <kushagrasingh1100@gmail.com>
2025-02-07 22:59:03 +0530

    final changes

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import "../styles/Teams.css";
 import Navbar from "./Navbar";
+import { MdOutlinePeopleAlt } from "react-icons/md";
+import { RiPencilLine } from "react-icons/ri";
+import { IoMdStats } from "react-icons/io";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+gsap.registerPlugin(useGSAP);
 const Teams = () => {
+  useGSAP(() => {
+    var tl = gsap.timeline();
+    tl.from(".teams-card ", {
+      y: 30,
+      duration: 0.3,
+      delay: 0.3,
+      opacity: 0,
+      stagger: 0.2,
+    });
+  });
   return (
     <div className="teams-page">
       <Navbar />
@@ -9,50 +26,85 @@ const Teams = () => {
         <div className="teams-cards">
           <div className="teams-card">
             <div className="teams-upper-one"></div>
-            <div className="teams-z">R</div>
+            <div className="teams-z teams-one">R</div>
             <div className="teams-lower">
-              <p>Rocket</p>
+              <h4>Rocket</h4>
               <p>Social Media Marketing</p>
+              <p>
+                <MdOutlinePeopleAlt /> 13 People
+              </p>
+              <IoMdStats /> &nbsp;
+              <RiPencilLine />
             </div>
           </div>
           <div className="teams-card">
             <div className="teams-upper-two"></div>
-            <div className="teams-z">R</div>
+            <div className="teams-z teams-two">R</div>
             <div className="teams-lower">
-              <p>Rocket</p>
+              <h4>Rocket</h4>
               <p>Social Media Marketing</p>
+              <p>
+                <MdOutlinePeopleAlt /> 13 People
+              </p>
+              <IoMdStats />
+              &nbsp;
+              <RiPencilLine />
             </div>
           </div>
           <div className="teams-card">
             <div className="teams-upper-three"></div>
-            <div className="teams-z">R</div>
+            <div className="teams-z teams-three">R</div>
             <div className="teams-lower">
-              <p>Rocket</p>
+              <h4>Rocket</h4>
               <p>Social Media Marketing</p>
+              <p>
+                <MdOutlinePeopleAlt /> 13 People
+              </p>
+              <IoMdStats />
+              &nbsp;
+              <RiPencilLine />
             </div>
           </div>
           <div className="teams-card">
             <div className="teams-upper-four"></div>
-            <div className="teams-z">R</div>
+            <div className="teams-z teams-four">R</div>
             <div className="teams-lower">
-              <p>Rocket</p>
+              <h4>Rocket</h4>
               <p>Web Dev</p>
+              <p>
+                <MdOutlinePeopleAlt /> 13 People
+              </p>
+              <IoMdStats />
+              &nbsp;
+              <RiPencilLine />
             </div>
           </div>
           <div className="teams-card">
             <div className="teams-upper-five"></div>
-            <div className="teams-z">R</div>
+            <div className="teams-z teams-five">R</div>
             <div className="teams-lower">
-              <p>Rocket</p>
+              <h4>Rocket</h4>
               <p>AI/ML</p>
+              <p>
+                <MdOutlinePeopleAlt /> 13 People
+              </p>
+              <IoMdStats />
+              &nbsp;
+              <RiPencilLine />
             </div>
           </div>
           <div className="teams-card">
             <div className="teams-upper-six"></div>
-            <div className="teams-z">R</div>
+            <div className="teams-z teams-six">R</div>
             <div className="teams-lower">
-              <p>Rocket</p>
+              <h4>Rocket</h4>
               <p>R & D</p>
+              <p>
+                <MdOutlinePeopleAlt /> 13 People
+              </p>
+              <IoMdStats />
+              &nbsp;
+              <RiPencilLine />
             </div>
           </div>
         </div>
